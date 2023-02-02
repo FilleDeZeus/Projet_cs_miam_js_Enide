4ca151e226ffc32aa0055ba1b69d261794e4703d
FilleDeZeus <enide26@hotmail.fr>
2023-02-02 16:53:03 +0100

    commentaire code

--- public/script/js/app.js
+++ public/script/js/app.js
@@ -1,11 +1,12 @@
-// ------------PARTIE JS ---------
+// ------------PARTIE MENU EN JS APPELLE DES FONCTIONS ---------
 import {menuDiner,menuBreakfast,menuLunch,menuStarters,lunchClick,startClick,breakClick,dinnerClick} from "./module/menu.js"
 lunchClick.addEventListener("click", menuLunch)
 startClick.addEventListener("click", menuStarters)
 breakClick.addEventListener("click", menuBreakfast)
 dinnerClick.addEventListener("click", menuDiner)
-
-//events carousel
+//-------------------------------------------------------
+// -------------------------CAROUSSEL-----------------------
+//-----------------EVENT CAROUSSEL----------
 let eventsCarouselBtn = document.querySelectorAll(".carouselBtnEvents > button");
 let eventsView = document.querySelector(".eventsView");
 
@@ -17,14 +18,12 @@ for (let i = 0; i < eventsCarouselBtn.length; i++) {
         eventsCarouselBtn[i].className="btnActiveEvents";
     })
 }
-//-------------------------------------------------------
-//events carousel
+
+//-------------GALLERY CAROUSSEL----------------
 let galleryCarouselBtn = document.querySelectorAll(".carouselBtnGallery > button");
 let galleryCarousel = document.querySelector(".galleryCarousel");
 let galleryItems = document.querySelectorAll(".galleryCarouselItem > img");
 
-// console.log(galleryCarouselBtn);
-// console.log(galleryItems);
 
 for (let i = 0; i < galleryCarouselBtn.length; i++) {
     galleryCarouselBtn[i].addEventListener("click", ()=>{
@@ -38,11 +37,10 @@ for (let i = 0; i < galleryCarouselBtn.length; i++) {
     })
 }
 
-//testimonial carousel
+//----------TESTIMONIAL CAROUSSEL-------------
 let testimonialCarouselBtn = document.querySelectorAll(".carouselBtn > button");
 let carouselItems = document.querySelector(".containerCarousel");
 
-//btn carousel testimonial
 for (let i = 0; i < testimonialCarouselBtn.length; i++) {
     testimonialCarouselBtn[i].addEventListener("click", ()=>{
         carouselItems.style.transform = `translateX(${-(testimonialCarouselBtn[i].value)*25}%)`;
@@ -52,29 +50,3 @@ for (let i = 0; i < testimonialCarouselBtn.length; i++) {
     })
 }
 
-//nav modal
-// Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-let btn = document.querySelector(".btnbooktable");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-} 
