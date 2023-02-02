b8b91a0540e03e3e29b4e53b38bb514e0a9b8b21
FilleDeZeus <enide26@hotmail.fr>
2023-02-02 16:48:47 +0100

    il me manque des commits

--- public/script/js/app.js
+++ public/script/js/app.js
@@ -51,3 +51,30 @@ for (let i = 0; i < testimonialCarouselBtn.length; i++) {
         testimonialCarouselBtn[i].className="btnActive";
     })
 }
+
+//nav modal
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.querySelector(".btnbooktable");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
