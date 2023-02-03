905580124489da9ef8f87c8c443bbd82a2835d9d
FilleDeZeus <enide26@hotmail.fr>
2023-02-03 12:33:07 +0100

    defilemen automatique

--- public/script/js/app.js
+++ public/script/js/app.js
@@ -1,5 +1,14 @@
 // ------------PARTIE MENU EN JS APPELLE DES FONCTIONS ---------
-import {menuDiner,menuBreakfast,menuLunch,menuStarters,lunchClick,startClick,breakClick,dinnerClick} from "./module/menu.js"
+import {
+    menuDiner,
+    menuBreakfast,
+    menuLunch,
+    menuStarters,
+    lunchClick,
+    startClick,
+    breakClick,
+    dinnerClick
+} from "./module/menu.js"
 lunchClick.addEventListener("click", menuLunch)
 startClick.addEventListener("click", menuStarters)
 breakClick.addEventListener("click", menuBreakfast)
@@ -12,10 +21,10 @@ let btnEvents = document.querySelectorAll(".btnEvents > button");
 let carouselEvent = document.querySelector(".carouselEvent");
 
 for (let i = 0; i < btnEvents.length; i++) {
-    btnEvents[i].addEventListener("click", ()=>{
+    btnEvents[i].addEventListener("click", () => {
         carouselEvent.style.transform = `translateX(${-(btnEvents[i].value)*20}%)`
-        document.querySelector(".btnActiveEvents").className="";
-        btnEvents[i].className="btnActiveEvents";
+        document.querySelector(".btnActiveEvents").className = "";
+        btnEvents[i].className = "btnActiveEvents";
     })
 }
 
@@ -23,16 +32,30 @@ for (let i = 0; i < btnEvents.length; i++) {
 
 //----------TESTIMONIAL CAROUSSEL-------------
 let btnTestiomonial = document.querySelectorAll(".carouselBtn > button");
+
 let testimonialCase = document.querySelector(".testimonialCase");
-
+let currentIndex = 0;
+let intervalId = null
 for (let i = 0; i < btnTestiomonial.length; i++) {
-    btnTestiomonial[i].addEventListener("click", ()=>{
+    btnTestiomonial[i].addEventListener("click", () => {
+        currentIndex = i;
         testimonialCase.style.transform = `translateX(${-(btnTestiomonial[i].value)*25}%)`;
-        document.querySelector(".btnActive").className="";
-        btnTestiomonial[i].className="btnActive";
-    })
+        document.querySelector(".btnActive").className = "";
+        btnTestiomonial[i].className = "btnActive";
+    });
 }
-
+function Auto () { 
+    intervalId = setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= btnTestiomonial.length) {
+    currentIndex = 0;
+    }
+    testimonialCase.style.transform = `translateX(${-(btnTestiomonial[currentIndex].value)*25}%)`;
+    document.querySelector(".btnActive").className = "";
+    btnTestiomonial[currentIndex].className = "btnActive";
+}, 4000);
+}
+Auto()
 //-------------GALLERY CAROUSSEL----------------
 let btnCarousel = document.querySelectorAll(".btnGallery > button");
 let carouselGallery = document.querySelector(".carouselGallery");
@@ -40,26 +63,56 @@ let galleryCase = document.querySelectorAll(".carouselGalleryCase > img");
 
 function troisiemeimage() {
     carouselGallery.style.transform = `translateX(${40 -(2)*20}%)`;
-    document.querySelector(".btnActiveGallery").className="";
-    btnCarousel[2].className="btnActiveGallery";
-    document.querySelector(".imgActiveGallery").className="";
-    galleryCase[2].className="imgActiveGallery";
-  }
+    document.querySelector(".btnActiveGallery").className = "";
+    btnCarousel[2].className = "btnActiveGallery";
+    document.querySelector(".imgActiveGallery").className = "";
+    galleryCase[2].className = "imgActiveGallery";
+}
 
-  troisiemeimage()
+troisiemeimage()
 
 for (let i = 0; i < btnCarousel.length; i++) {
 
-    btnCarousel[i].addEventListener("click", ()=>{
+    btnCarousel[i].addEventListener("click", () => {
 
         carouselGallery.style.transform = `translateX(${40 -(btnCarousel[i].value)*20}%)`
-        document.querySelector(".btnActiveGallery").className="";
-        btnCarousel[i].className="btnActiveGallery";
-        document.querySelector(".imgActiveGallery").className="";
-        galleryCase[i].className="imgActiveGallery"
+        document.querySelector(".btnActiveGallery").className = "";
+        btnCarousel[i].className = "btnActiveGallery";
+        document.querySelector(".imgActiveGallery").className = "";
+        galleryCase[i].className = "imgActiveGallery"
     })
 }
 
-// carouselGallery.style.marginLeft = "-900px"
+//----DARK MODE-----------
 
-// console.log(carouselGallery)
+let darkMode = document.getElementById("darkMode");
+let body = document.body;
+
+darkMode.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        darkMode.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+    } else {
+        darkMode.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+    }
+});
+
+//----------MODAL----------
+let btnbooktable = document.querySelector(".btnbooktable");
+let bookingModal = document.getElementById("bookingModal");
+let close = document.querySelector(".close");
+
+btnbooktable.addEventListener("click", function () {
+    bookingModal.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+    bookingModal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target == bookingModal) {
+        bookingModal.style.display = "none";
+    }
+});
