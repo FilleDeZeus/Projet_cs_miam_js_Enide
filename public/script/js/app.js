3a2eb5be14d22da704db6c5d543d87931fd19e67
FilleDeZeus <enide26@hotmail.fr>
2023-02-03 13:17:24 +0100

    troisiéme section

--- public/script/js/app.js
+++ public/script/js/app.js
@@ -16,19 +16,6 @@ dinnerClick.addEventListener("click", menuDiner)
 
 //-------------------------------------------------------
 // -------------------------CAROUSSEL-----------------------
-//-----------------EVENT CAROUSSEL----------
-let btnEvents = document.querySelectorAll(".btnEvents > button");
-let carouselEvent = document.querySelector(".carouselEvent");
-
-for (let i = 0; i < btnEvents.length; i++) {
-    btnEvents[i].addEventListener("click", () => {
-        carouselEvent.style.transform = `translateX(${-(btnEvents[i].value)*20}%)`
-        document.querySelector(".btnActiveEvents").className = "";
-        btnEvents[i].className = "btnActiveEvents";
-    })
-}
-
-
 
 //----------TESTIMONIAL CAROUSSEL-------------
 let btnTestiomonial = document.querySelectorAll(".carouselBtn > button");
@@ -44,7 +31,7 @@ for (let i = 0; i < btnTestiomonial.length; i++) {
         btnTestiomonial[i].className = "btnActive";
     });
 }
-function Auto () { 
+function Auto1 () { 
     intervalId = setInterval(() => {
     currentIndex++;
     if (currentIndex >= btnTestiomonial.length) {
@@ -55,11 +42,40 @@ function Auto () {
     btnTestiomonial[currentIndex].className = "btnActive";
 }, 4000);
 }
+Auto1()
+
+//-----------------EVENT CAROUSSEL----------
+let btnEvents = document.querySelectorAll(".btnEvents > button");
+let carouselEvent = document.querySelector(".carouselEvent");
+
+for (let i = 0; i < btnEvents.length; i++) {
+    btnEvents[i].addEventListener("click", () => {
+        carouselEvent.style.transform = `translateX(${-(btnEvents[i].value)*20}%)`
+        document.querySelector(".btnActiveEvents").className = "";
+        btnEvents[i].className = "btnActiveEvents";
+    })
+}
+let currentSlide = 0;
+let slideInterval = null;
+function Auto() {
+    slideInterval = setInterval(() => {
+    carouselEvent.style.transform = `translateX(${-(currentSlide + 1)*20}%)`;
+    document.querySelector(".btnActiveEvents").className = "";
+    btnEvents[currentSlide + 1].className = "btnActiveEvents";
+    if (currentSlide === btnEvents.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+}, 5000);}
 Auto()
 //-------------GALLERY CAROUSSEL----------------
 let btnCarousel = document.querySelectorAll(".btnGallery > button");
 let carouselGallery = document.querySelector(".carouselGallery");
 let galleryCase = document.querySelectorAll(".carouselGalleryCase > img");
+let currentIndex1 = 0;
+
+let intervalId1 = null;
 
 function troisiemeimage() {
     carouselGallery.style.transform = `translateX(${40 -(2)*20}%)`;
@@ -83,6 +99,21 @@ for (let i = 0; i < btnCarousel.length; i++) {
     })
 }
 
+function Auto2() {
+    intervalId1 = setInterval(() => {
+
+        currentIndex1++;
+        if (currentIndex1 >= btnCarousel.length) {
+            currentIndex1 = 0 ;
+        }
+        carouselGallery.style.transform = `translateX(${40 -(btnCarousel[currentIndex1].value)*20}%)`;
+        document.querySelector(".btnActiveGallery").className = "";
+        btnCarousel[currentIndex1].className = "btnActiveGallery";
+        document.querySelector(".imgActiveGallery").className = "";
+        galleryCase[currentIndex1].className = "imgActiveGallery";
+    }, 3000);
+}
+Auto2()
 //----DARK MODE-----------
 
 let darkMode = document.getElementById("darkMode");
